7fa82979941c3b43797af3b5fe7adad804385452
Mustafa Fares <mustaphafares445@gmail.com>
2026-08-21 22:44:54 +0300

    Make notification submit action mobile friendly

--- resources/views/filament/pages/send-notifications.blade.php
+++ resources/views/filament/pages/send-notifications.blade.php
@@ -1,9 +1,9 @@
 <x-filament-panels::page>
-    <form wire:submit="send" class="space-y-6">
+    <form wire:submit="send" class="min-w-0 space-y-6">
         {{ $this->form }}
 
-        <div class="flex justify-end">
-            <x-filament::button type="submit" icon="heroicon-o-paper-airplane">
+        <div class="flex min-w-0 justify-stretch sm:justify-end">
+            <x-filament::button type="submit" icon="heroicon-o-paper-airplane" class="w-full sm:w-auto">
                 Send Notification
             </x-filament::button>
         </div>
