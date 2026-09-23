00d76171d0e53b422b2094e74c0615afccfb12ee
Mustafa Fares <mustaphafares445@gmail.com>
2026-09-23 16:16:45 +0300

    Default new dashboard users to English admin accounts

--- app/Filament/Resources/Users/Pages/CreateUser.php
+++ app/Filament/Resources/Users/Pages/CreateUser.php
@@ -3,9 +3,27 @@
 namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
+use App\Models\User;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['preferred_language'] = 'en';
+
+        return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        /** @var User $record */
+        $record = $this->getRecord();
+
+        if (! $record->hasRole('admin')) {
+            $record->assignRole('admin');
+        }
+    }
 }
