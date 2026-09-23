9a71fc46a91f1a72d93d5c7d1f9bb251c4d4f65b
Mustafa Fares <mustaphafares445@gmail.com>
2026-09-23 17:50:15 +0300

    Count grouped app items correctly in catalog stats

--- app/Filament/Resources/Items/Widgets/ItemCatalogStats.php
+++ app/Filament/Resources/Items/Widgets/ItemCatalogStats.php
@@ -4,8 +4,10 @@ namespace App\Filament\Resources\Items\Widgets;
 
 use App\Models\Item;
 use App\Models\ItemFilterMapping;
+use App\Services\Mobile\ItemApiSettingsService;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Database\Eloquent\Builder;
 
 class ItemCatalogStats extends StatsOverviewWidget
 {
@@ -13,9 +15,8 @@ class ItemCatalogStats extends StatsOverviewWidget
 
     protected function getStats(): array
     {
-        $shownInApp = Item::query()
-            ->apiVisible()
-            ->count();
+        $groupedMode = app(ItemApiSettingsService::class)->uniqueSerialItemsEnabled();
+        $shownInApp = $this->shownInAppCount($groupedMode);
 
         $needsReview = Item::query()
             ->whereHas(
@@ -33,7 +34,9 @@ class ItemCatalogStats extends StatsOverviewWidget
 
         return [
             Stat::make('Shown in app', number_format($shownInApp))
-                ->description('Items currently available to customers')
+                ->description($groupedMode
+                    ? 'Unique serial items currently available to customers'
+                    : 'Items currently available to customers')
                 ->icon('heroicon-o-eye')
                 ->color('success'),
             Stat::make('Needs review', number_format($needsReview))
@@ -45,5 +48,29 @@ class ItemCatalogStats extends StatsOverviewWidget
                 ->icon('heroicon-o-photo')
                 ->color('danger'),
         ];
+    }
+
+    private function shownInAppCount(bool $groupedMode): int
+    {
+        $query = Item::query()->apiVisible();
+
+        if (! $groupedMode) {
+            return $query->count();
+        }
+
+        $uniqueSerials = (clone $query)
+            ->whereNotNull('normalized_serial')
+            ->where('normalized_serial', '!=', '')
+            ->distinct()
+            ->count('normalized_serial');
+
+        $itemsWithoutSerial = (clone $query)
+            ->where(function (Builder $query): void {
+                $query->whereNull('normalized_serial')
+                    ->orWhere('normalized_serial', '');
+            })
+            ->count();
+
+        return $uniqueSerials + $itemsWithoutSerial;
     }
 }
